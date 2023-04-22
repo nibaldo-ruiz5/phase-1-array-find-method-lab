5e5883841d47fade125f54a9adf9bedf460cd115
Nibaldo Ruiz <nibaldo78@hotmail.com>
2023-04-22 17:34:20 -0400

    with your message here

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+    return winningGame ? winningGame.year : undefined;
+  }
+  
